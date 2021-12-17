932ba6cb5a22ebb201583ca9b8343d6f25848240
liisamalm <90306856+liisamalm@users.noreply.github.com>
2021-12-17 09:22:35 +0200

    päivitys vielä

--- venues/js/venueskriptit.js
+++ venues/js/venueskriptit.js
@@ -4,12 +4,12 @@ var mini = true;
 
 function toggleSidebar() {
   if (mini) {
-    console.log("opening sidebar");
+    //console.log("opening sidebar");
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     this.mini = false;
   } else {
-    console.log("closing sidebar");
+    //console.log("closing sidebar");
     document.getElementById("mySidebar").style.width = "85px";
     document.getElementById("main").style.marginLeft = "85px";
     this.mini = true;
